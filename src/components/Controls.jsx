b5988979708ded82ecc3c56b805a989752edfda8
Mohammad Hamza Khan <mohdhamzakhan2005@gmail.com>
2025-03-06 13:21:39 +0530

    Updated game-3

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -5,19 +5,19 @@ const Controls = ({ updateGrid, toggleAutoPlay, isRunning, clearGrid }) => {
     <div className="controls ">
       <button
         onClick={updateGrid}
-        className="controls-toggle p-4 m-4 rounded-full bg-cyan-500 shadow-lg shadow-cyan-500/50 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 "
+        className="controls-toggle p-4 m-4 rounded-full bg-[#FFD700] shadow-lg shadow-cyan-500/50 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 "
       >
         Start
       </button>
       <button
         onClick={toggleAutoPlay}
-        className="bg-blue-500 p-4 m-4 rounded-full shadow-lg shadow-blue-500/50 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 "
+        className="bg-[#FFB300] p-4 m-4 rounded-full shadow-lg shadow-blue-500/50 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 "
       >
         {isRunning ? "Stop AutoPlay" : "Start AutoPlay"}
       </button>
       <button
         onClick={clearGrid}
-        className="bg-indigo-500 p-4 m-4 rounded-full shadow-lg shadow-indigo-500/50 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 "
+        className="bg-[#FFD700] p-4 m-4 rounded-full shadow-lg shadow-indigo-500/50 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 "
       >
         Clear
       </button>
